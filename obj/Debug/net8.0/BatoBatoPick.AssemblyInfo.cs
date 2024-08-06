44404ebbb3b69b2805ff7840a322f2a645ff7013
JULZARTS <ignacio221julius@gmail.com>
2024-08-06 22:54:31 +0800

    Final branch

--- obj/Debug/net8.0/BatoBatoPick.AssemblyInfo.cs
+++ obj/Debug/net8.0/BatoBatoPick.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2287af48a7bc3ed0e27dad48c162bf6a09336081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8203afada4e58ca63850167ea443831b6d72db6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
