90c14d0f26c5fb5d93788fc8a3b223ec67bea49a
JULZARTS <ignacio221julius@gmail.com>
2024-08-07 00:53:53 +0800

    ______

--- obj/Debug/net8.0/BatoBatoPick.AssemblyInfo.cs
+++ obj/Debug/net8.0/BatoBatoPick.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050ca752ada745f5d67a187be11b651ef42ff8a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fd1258ffaf155fa788cf93872a7598be848a11")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
