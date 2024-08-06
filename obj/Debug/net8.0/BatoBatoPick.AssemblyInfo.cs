2287af48a7bc3ed0e27dad48c162bf6a09336081
JULZARTS <ignacio221julius@gmail.com>
2024-08-06 16:53:39 +0800

    Added a background image on starting screen

--- obj/Debug/net8.0/BatoBatoPick.AssemblyInfo.cs
+++ obj/Debug/net8.0/BatoBatoPick.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ccb276b0297d5a533e6c931d618818fe7c4afe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb780171e1b525b8994aff155db8744646fa916")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatoBatoPick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
